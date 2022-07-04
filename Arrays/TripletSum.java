b2eb2794be8d05dd9110a76ccc5c4402860fce0e
Sunidhi Singh <96500508+sunidhi014@users.noreply.github.com>
2022-07-04 11:46:13 +0530

    Create TripletSum.java

---
+++ Arrays/TripletSum.java
@@ -0,0 +1,25 @@
+/* You have been given a random integer array/list(ARR) and a number X. Find and return the number of triplets in the array/list which sum to X. */
+
+import java.util.*;
+public class Solution {
+    
+    public static int findTriplet(int[] arr, int x) {
+    	int count=0;
+        for (int i=0;i<arr.length-2;i++)
+        {
+            for (int j=i+1;j<arr.length-1;j++)
+            {
+                for (int k=j+1;k<arr.length;k++)
+                {
+                    if (arr[i]+arr[j]+arr[k]==x)
+                        {
+                            count++;
+                        }
+                }
+            }
+        }
+        
+        return (count);
+    }
+
+}
