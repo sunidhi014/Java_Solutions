b0217ec54c8ad73f86067439afd28d78e306d54b
Sunidhi Singh <96500508+sunidhi014@users.noreply.github.com>
2022-11-06 11:24:35 +0530

    Create Sum of nodes.java

---
+++ Trees/Sum of nodes.java
@@ -0,0 +1,29 @@
+/* Given a generic tree, count and return the sum of all nodes present in the given tree. */
+
+public class Solution {
+
+/*	TreeNode structure 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
+	
+	
+	public static int sumOfAllNode(TreeNode<Integer> root){
+		if(root == null)
+		return Integer.MIN_VALUE;
+
+		int sum=0;
+		for(int i=0;i<root.children.size();++i){
+			sum+=sumOfAllNode(root.children.get(i));
+		}
+		return root.data+sum;
+	}
+}
