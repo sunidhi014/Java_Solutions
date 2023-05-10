020113ad4450523195136f4575b312d907cece3a
Sunidhi Singh <96500508+sunidhi014@users.noreply.github.com>
2023-05-10 11:51:58 +0530

    Update code.java

--- Dynamic Programming/Snake and Ladder/code.java
+++ Dynamic Programming/Snake and Ladder/code.java
@@ -4,52 +4,108 @@ You are given an integer N denoting the total number of snakes and ladders and a
 and (2*i + 1)th values denote the starting and ending point respectively of ith snake or ladder. The board looks like the following.
 Note: Assume that you have complete control over the 6 sided dice. No ladder starts from 1st cell. */
 
+
+
+// User function Template for Java
+
 class Solution{
-    static class qentry {
-        int v; // Vertex number
-        int dist; // Distance of this vertex from source
-    }
+    static int minThrow(int n, int arr[]) {
 
-    static int minThrow(int n, int move[]) {
-        int visited[] = new int[n];
-        Queue<qentry> q = new LinkedList<>();
-        qentry qe = new qentry();
-        qe.v = 0;
-        qe.dist = 0;
+        // Create a 1D array of the board with size 31
 
-        // Mark the node 0 as visited and enqueue it.
-        visited[0] = 1;
-        q.add(qe);
+        int[] board = new int[31];
 
-        // Do a BFS starting from vertex at index 0
-        while (!q.isEmpty()) {
-            qe = q.remove();
-            int v = qe.v;
+        // Fill all values in the board with -1
 
-            // If front vertex is the destination vertex, we are done
-            if (v == n - 1)
-                break;
+        Arrays.fill(board, -1);
 
-            // Otherwise, dequeue the front vertex and enqueue its adjacent vertices (or cell
-            // numbers reachable through a dice throw)
-            for (int j = v + 1; j <= v + 6 && j < n; ++j) {
-                // If this cell is already visited, then ignore
-                if (visited[j] == 0) {
-                    // Otherwise, calculate its distance and mark it as visited
-                    qentry a = new qentry();
-                    a.dist = qe.dist + 1;
-                    visited[j] = 1;
+        // Loop through the input array and update the board with special cells
 
-                    // Check if there is a ladder or snake at 'j'
-                    // If so, set the destination as the adjacent cell
-                    if (move[j] != -1)
-                        a.v = move[j];
-                    else
-                        a.v = j;
-                    q.add(a);
-                }
-            }
+        for (int i = 0; i <= arr.length - 2; i += 2) {
+
+            board[arr[i]] = arr[i + 1];
+
         }
-        return qe.dist;
-    }
+
+        // Create a queue to store cells to be visited
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Offer cell 1 to the queue
+
+        queue.offer(1);
+
+        // Create a boolean array to keep track of visited cells
+
+        boolean[] visited = new boolean[31];
+
+        // Initialize a variable to keep track of number of dice throws
+
+        int move = 0;
+
+        // Keep looping until the queue is empty
+
+        while (!queue.isEmpty()) {
+
+            // Get the number of elements in the queue
+
+            int qsize = queue.size();
+
+            // Loop through all elements in the queue
+
+            for (int size = qsize; size > 0; size--) {
+
+                // Get the next cell from the queue
+
+                int num = queue.poll();
+
+                // If the cell has been visited, continue to the next iteration
+
+                if (visited[num])
+
+                    continue;
+
+                // Mark the cell as visited
+
+                visited[num] = true;
+
+                // Check if the cell is cell 30
+
+                if (num == 30)
+
+                    return move;
+
+                // Loop through the possible dice throws (1 to 6)
+
+                for (int i = 1; i <= 6 && num + i <= 30; i++) {
+
+                    int next = num + i;
+
+                    // Check if the next cell has a special value in the board
+
+                    if (board[next] != -1)
+
+                        next = board[next];
+
+                    // If the next cell has not been visited, offer it to the queue
+
+                    if (!visited[next])
+
+                        queue.offer(next);
+
+                }
+
+            }
+
+            // Increment the number of dice throws
+
+            move++;
+
+        }
+
+        // Return -1 if cell 30 could not be reached
+
+        return -1;
+
+    } 
 }
